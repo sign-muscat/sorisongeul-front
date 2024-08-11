b20a97223c9cdb408f73b1c8ca176ea1df3098e5
ddalla0425 <hiwgod92@gmail.com>
2024-08-11 22:28:22 +0900

    chose: files and codes unrelated to the current branch delete

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,5 +1,4 @@
 import { Box, Divider, HStack } from "@chakra-ui/react";
-import { useNavigate } from "react-router-dom";
 import GameCard from "../components/card/GameCard";
 
 function MainPage() {
