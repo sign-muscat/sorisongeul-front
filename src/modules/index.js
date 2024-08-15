88397ba7979b2300c76dc0aa06b2e2a8b166a8dc
ieepp <6076hs@gmail.com>
2024-08-15 21:21:37 +0900

    feature: implement friend api calls

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,9 +3,10 @@ import handGameReducer from "./HandGameReducer";
 import rankReducer from "./RankReducer";
 import soundGameReducer from "./SoundGameReducer";
 import lipGameReducer from "./LipGameReducer";
+import friendReducer from "./FriendReducer";
 
 const rootReducer = combineReducers({
-    handGameReducer, soundGameReducer, rankReducer, lipGameReducer
+    handGameReducer, soundGameReducer, rankReducer, lipGameReducer, friendReducer
 });
 
 export default rootReducer;
