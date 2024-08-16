c45717e7c877f55ddda0d02b7078c7b090500aa7
ppyye <154950167+ppyye@users.noreply.github.com>
2024-08-16 09:27:39 +0900

    #27 친구 관련 컴포넌트 구현 (#31)
    * layout: friend card
    
    * feature: implement friend api calls
    
    * feature: implement friend api calls
    
    * layout: add friend button

---
+++ src/components/card/FriendApplyCard.js
@@ -0,0 +1,50 @@
+import {
+    Avatar, Text, Box, Card, Flex, Heading, useDisclosure, Button, VStack
+} from "@chakra-ui/react";
+import {SmallCloseIcon} from "@chakra-ui/icons";
+import {useRef} from "react";
+import {useDispatch} from "react-redux";
+import {callHandleFriendRequestAPI} from "../../apis/FriendAPICalls";
+
+function FriendApplyCard({apply}) {
+
+    const dispatch = useDispatch();
+    const acceptRequest = () => {
+        dispatch(callHandleFriendRequestAPI(apply.friendId, 'ACCEPTED'));
+    }
+
+    const rejectRequest = () => {
+        dispatch(callHandleFriendRequestAPI(apply.friendId, 'REJECTED'));
+    }
+
+    return (
+        apply &&
+            <>
+                <Card maxW='xs'>
+                    <Flex spacing='4'>
+                        <Flex p={4} flex='1' gap='4' alignItems='center' flexWrap='wrap'>
+                            <Avatar name={apply.nickname} src={apply.profileImage} />
+
+                            <Box>
+                                <Heading size='sm'>{apply.nickname}</Heading>
+                                <Text fontSize='12px'>{apply.applyDate}</Text>
+                            </Box>
+                            <VStack>
+                                <Button colorScheme='mint' size='xs' borderRadius='full' onClick={acceptRequest}>
+                                    수락
+                                </Button>
+                                <Button colorScheme='gray' size='xs' borderRadius='full' onClick={rejectRequest}>
+                                    거절
+                                </Button>
+
+                            </VStack>
+                        </Flex>
+
+                    </Flex>
+                </Card>
+            </>
+
+    );
+}
+
+export default FriendApplyCard;
