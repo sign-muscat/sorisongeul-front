3e5751b6029c79362c4509d627798b559062cfed
ddalla0425 <hiwgod92@gmail.com>
2024-08-19 04:41:39 +0900

    chose: modify not used import

--- src/components/router/ProtectedRoute.js
+++ src/components/router/ProtectedRoute.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { isLogin, isAdmin, isFreeUser, isPremiumUser } from '../../utils/TokenUtils';
 
 function ProtectedRoute({ isAuthenticated, isAdminOnly, role, onRequireLogin, children }) {
