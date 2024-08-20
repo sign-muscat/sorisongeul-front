2714c26a026a6521c75db1ae8a9220c50ae4dcb4
ddalla0425 <hiwgod92@gmail.com>
2024-08-20 12:40:49 +0900

    chose: annotation delete

--- src/App.js
+++ src/App.js
@@ -1,33 +1,3 @@
-// import { Routes, Route } from 'react-router-dom';
-// import MainLayout from './layouts/MainLayout.js';
-// import MainPage from "./pages/MainPage";
-// import HandGameInfo from "./pages/hand/HandGameInfo";
-// import SoundGameInfo from "./pages/sound/SoundGameInfo";
-// import LipGameInfo from "./pages/lip/LipGameInfo";
-// import WideLayout from "./layouts/WideLayout";
-// import PricingPage from "./pages/pricing/PricingPage";
-// import ProtectedRoute from "./components/router/ProtectedRoute";
-//
-// function App() {
-//     return (
-//         <Routes>
-//             <Route path="/" element={<MainLayout/>}>
-//                 <Route index element={<MainPage/>}/>
-//                 <Route path="game">
-//                     <Route path="hand" element={<HandGameInfo/>}/>
-//                     <Route path="sound" element={<SoundGameInfo/>}/>
-//                     <Route path="lip" element={<LipGameInfo/>}/>
-//                 </Route>
-//             </Route>
-//             <Route path="/pricing" element={<WideLayout/>}>
-//                 <Route index element={<PricingPage/>}/>
-//             </Route>
-//         </Routes>
-//     );
-// }
-//
-// export default App;
-
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
