9b1b357ffa2512c8c7f47d3ad853584edb089eb6
ppyye <6076hs@gmail.com>
2024-08-26 12:14:13 +0900

    layout: modify card width

--- src/components/card/GameCard.js
+++ src/components/card/GameCard.js
@@ -1,15 +1,15 @@
-import {Box, Card, Text, Link as ChakraLink, Image} from "@chakra-ui/react";
+import {Box, Card, Text, Link as ChakraLink, Image, Divider} from "@chakra-ui/react";
 import { Link as ReactRouterLink } from 'react-router-dom'
 
 function GameCard({game}) {
     return (
         <ChakraLink as={ReactRouterLink} to={game.url}>
-            <Card w={175} h={270} p={6}>
-                <Box>
+            <Card w={200} p={6}>
+                <Box p={2}>
                     <Image src={game.img}/>
                 </Box>
-                <Text fontWeight={700} fontSize='20px'>{game.title}</Text>
-                <Text>{game.desc}</Text>
+                <Text fontWeight={700} fontSize='20px' pt={2} color='gray.700'>{game.title}</Text>
+                <Text color='gray.700'>{game.desc}</Text>
             </Card>
         </ChakraLink>
     );
